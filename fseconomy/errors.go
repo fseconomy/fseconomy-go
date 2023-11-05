09c4ce9f709a4517ddf7d5319e5aed161b489cd3
daemotron <mail@daemotron.net>
2023-11-05 17:50:24 +0100

    new error for invalid service name

--- fseconomy/errors.go
+++ fseconomy/errors.go
@@ -7,4 +7,7 @@ import (
 var (
 	// ErrBadKey malformed key
 	ErrBadKey = errors.New("malformed key")
+
+	// ErrInvalidService service doesn't exist
+	ErrInvalidService = errors.New("service does not exist")
 )
