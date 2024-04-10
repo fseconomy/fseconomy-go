b30b49894ea2874c2456e8c603d897e0fae6d1bc
daemotron <mail@daemotron.net>
2024-04-10 19:43:42 +0200

    adapt to new structure

--- internal/api/query.go
+++ internal/api/query.go
@@ -1,17 +1,17 @@
 package api
 
 import (
-	"github.com/fseconomy/fseconomy-go/fseconomy"
+	"github.com/fseconomy/fseconomy-go/internal/core"
 	"net/http"
 	"net/url"
 )
 
-func Query(context *fseconomy.Fse, service *Service) {
+func Query(context *core.Fse, service *Service) {
 	path, err := url.JoinPath(service.api, service.url)
 	if err != nil {
 		return
 	}
 	req, err := http.NewRequest(service.method, path, nil)
 	client := http.Client{}
-	resp, err := client.Do(req)
+	_, _ = client.Do(req)
 }
