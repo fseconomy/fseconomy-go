45401d34a0c3d1e3277fc0fc51b4a62dd013f066
daemotron <mail@daemotron.net>
2024-04-12 22:13:12 +0200

    basic raw query function

--- internal/api/query.go
+++ internal/api/query.go
@@ -1,17 +1,58 @@
 package api
 
 import (
-	"github.com/fseconomy/fseconomy-go/internal/core"
+	"bytes"
+	"io"
 	"net/http"
 	"net/url"
 )
 
-func Query(context *core.Fse, service *Service) {
-	path, err := url.JoinPath(service.Api, service.Url)
-	if err != nil {
-		return
+func (service *Service) RawQuery(urlParams map[string]string, formParams map[string]string, headerParams map[string]string) ([]byte, error) {
+	// prepare url parameters
+	up := url.Values{}
+	for k, v := range urlParams {
+		up.Set(k, v)
 	}
-	req, err := http.NewRequest(service.Method, path, nil)
+
+	// prepare form parameters
+	fp := url.Values{}
+	for k, v := range formParams {
+		fp.Add(k, v)
+	}
+
+	// build final url from service data and url parameters
+	reqUrl, err := url.Parse(service.Api + service.Url)
+	if err != nil {
+		return nil, err
+	}
+	reqUrl.RawQuery = up.Encode()
+
+	// create request
+	req, err := http.NewRequest(service.Method, reqUrl.String(), bytes.NewBufferString(fp.Encode()))
+	if err != nil {
+		return nil, err
+	}
+
+	// set header params
+	// Set the Content-Type header for form data
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	for k, v := range headerParams {
+		req.Header.Set(k, v)
+	}
+
+	// create a http client and execute the request
 	client := http.Client{}
-	_, _ = client.Do(req)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	rawData, readErr := io.ReadAll(resp.Body)
+	closeErr := resp.Body.Close()
+	if readErr != nil {
+		return nil, readErr
+	}
+	if closeErr != nil {
+		return nil, closeErr
+	}
+	return rawData, nil
 }
